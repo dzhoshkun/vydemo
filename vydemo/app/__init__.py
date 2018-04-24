b0cb9f41e7c2209d243c22d6919ceebeca316dc6
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-04-24 15:20:42 +0100

    Issue #3: a first implementation of sample app

--- vydemo/app/__init__.py
+++ vydemo/app/__init__.py
@@ -1,8 +1,17 @@
 import os.path
 from pkg_resources import resource_filename
+from vydemo.gui import (VideoPlayer, App)
+from vydemo.giftgrab import ColourSpace
+from ._local_video_reader import LocalVideoReader
 
 
 def let_the_sun_shine():
     filepath = resource_filename(__name__,
                                  os.path.join('data', 'sunny-river.mp4'))
-    print('Let the sun shine on {}'.format(filepath))
+    video_reader = LocalVideoReader(filepath)
+    video_player = VideoPlayer(color=ColourSpace.BGRA, size=(480, 720))
+    video_reader.attach(video_player)
+    video_reader.start()
+    App(video_player).run()
+    video_reader.stop()
+    video_reader.detach(video_player)
