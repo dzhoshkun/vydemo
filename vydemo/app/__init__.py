09795b1cdcb0f3de2df1f00dfb993a60985c263f
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-04-24 11:11:42 +0100

    Issue #3: added data folder name for pkg resource retrieval

--- vydemo/app/__init__.py
+++ vydemo/app/__init__.py
@@ -1,6 +1,8 @@
+import os.path
 from pkg_resources import resource_string
 
 
 def let_the_sun_shine():
-    filepath = resource_string(__name__, 'sunny-river.mp4')
+    filepath = resource_string(__name__,
+                               os.path.join('data', 'sunny-river.mp4'))
     print('Let the sun shine on {}'.format(filepath))
