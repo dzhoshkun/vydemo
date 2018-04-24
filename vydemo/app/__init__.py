9cd4c3b51da2d00927f64248d7f258c41ba15d45
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-04-24 10:53:31 +0100

    Issue #3: testing retrieval of video file path at runtime from within console script

--- vydemo/app/__init__.py
+++ vydemo/app/__init__.py
@@ -1,2 +1,6 @@
+from pkg_resources import resource_string
+
+
 def let_the_sun_shine():
-    print('Let the sun shine')
+    filepath = resource_string(__name__, 'sunny-river.mp4')
+    print('Let the sun shine on {}'.format(filepath))
