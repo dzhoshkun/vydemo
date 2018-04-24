ebc576906fccf9015c9854edc44bab26030ef8f0
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-04-24 12:29:37 +0100

    Issue #3: exposed GIFT-Grab classes from vydemo.giftgrab package

--- vydemo/giftgrab/__init__.py
+++ vydemo/giftgrab/__init__.py
@@ -1,1 +1,11 @@
 """GIFT-Grab wrapper for systems where GIFT-Grab is not installed."""
+try:
+    from pygiftgrab import ColourSpace
+    from pygiftgrab import IObserver
+    from pygiftgrab import IObservable
+    from pygiftgrab import VideoFrame
+except ImportError:
+    from ._colour_space import ColourSpace
+    from ._iobserver import IObserver
+    from ._iobservable import IObservable
+    from ._video_frame import VideoFrame
